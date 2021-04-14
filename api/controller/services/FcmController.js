322b714cb07cf6e01e62fbc77a9d951318a36fdd
thabares <thabaresnazeer.1995@gmail.com>
2021-04-14 18:46:47 +0530

    added aps controller and added sound in fcm controller

--- api/controller/services/FcmController.js
+++ api/controller/services/FcmController.js
@@ -1,32 +1,34 @@
-var request = require("request");
+var request = require('request');
 
 module.exports = {
   sendFcmNotification: async function (fcmId, notification) {
     // this is to send fcm notification, start here
     var optionsFcm = {
-      url: "https://fcm.googleapis.com/fcm/send",
-      method: "POST",
+      url: 'https://fcm.googleapis.com/fcm/send',
+      method: 'POST',
       headers: {
-        "User-Agent": "Super Agent/0.0.1",
-        "Content-Type": "application/json",
+        'User-Agent': 'Super Agent/0.0.1',
+        'Content-Type': 'application/json',
         Authorization:
-          "key=AAAA93SG2KQ:APA91bG4T6QoVnPVLfQJAC095IjJ1mlNqH6jKzzt1EZAgfg9C8NviacUJbTBDmBQ-QM0owYSrL2fPHEOinSZE6mI1JqLlWclLbAGSBbbRxmVKI_oaPQaTGXcZbvmGX5tEz13Kx5NqPDK",
+          'key=AAAA93SG2KQ:APA91bG4T6QoVnPVLfQJAC095IjJ1mlNqH6jKzzt1EZAgfg9C8NviacUJbTBDmBQ-QM0owYSrL2fPHEOinSZE6mI1JqLlWclLbAGSBbbRxmVKI_oaPQaTGXcZbvmGX5tEz13Kx5NqPDK',
       },
       body: JSON.stringify({
         notification: {
           icon:
-            "https://ovaltine.s3.ap-south-1.amazonaws.com/LOGO_Ovantine-compressed.jpg",
+            'https://ovaltine.s3.ap-south-1.amazonaws.com/LOGO_Ovantine-compressed.jpg',
           title: notification.title,
           body: notification.body,
+          sound: 'default',
           id: Math.floor(Math.random() * 100 + 1),
         },
         data: {
           title: notification.title,
           body: notification.body,
           icon:
-            "https://ovaltine.s3.ap-south-1.amazonaws.com/LOGO_Ovantine-compressed.jpg",
+            'https://ovaltine.s3.ap-south-1.amazonaws.com/LOGO_Ovantine-compressed.jpg',
           poster: notification.poster,
           type: notification.type,
+          sound: 'default',
           actionId: notification.actionId,
           actionTitle: notification.actionTitle,
           url: notification.url,
